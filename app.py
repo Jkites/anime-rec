04f8a01f7f2adb4610933e0d81da738f89b7e1fd
Jkites <99958936+Jkites@users.noreply.github.com>
2025-08-07 21:07:27 -0700

    file structure

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+import pandas as pd
+# !!!
+# from recommender.model import recommend_for_user
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        file = request.files['file']
+        new_user_df = pd.read_csv(file)
+        # recommendations = recommend_for_user(new_user_df)
+        recommendations = None
+        return render_template('index.html', recs=recommendations)
+    return render_template('index.html', recs=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
